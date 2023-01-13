0cf91d4ea2916b6192ecbf3353e70689bf44dc21
JeremyWesthead <jeremywesthead3@gmail.com>
2023-01-13 13:36:49 +0000

    Test fixes + add pub key to stack for testing

--- tests/update_expected_short.bash
+++ tests/update_expected_short.bash
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-tests="ont_artic_short
-ont_viridian_short
-illumina_artic_short
-illumina_viridian_short"
+tests="gpas_ont_short
+gpas_illumina_short"
 
 for test_name in ${tests}
 do
     cp /work/output/${test_name}_test/${test_name}_summary.tsv \
-	    /data/pipelines/ncov2019-artic-nf/tests/${test_name}_expected.tsv
+	    /data/pipelines/SARS-CoV2_workflows/tests/${test_name}_expected.tsv
 done
 
